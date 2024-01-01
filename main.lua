1cccdfcb918808037bf672e62bd5732d269dc676
Zazou89 <155338110+Zazou89@users.noreply.github.com>
2024-01-01 03:18:09 +0100

    Initial Release

---
+++ main.lua
@@ -0,0 +1,46 @@
+---@class BetterBags: AceAddon
+local addon = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
+
+---@class Categories: AceModule
+local categories = addon:GetModule('Categories')
+
+---@class Localization: AceModule
+local L = addon:GetModule('Localization')
+
+--Dragonscale Expedition
+categories:AddItemToCategory(200285, L:G("Reputation Insignia")) --Dragonscale Expedition Insignia (Green)
+categories:AddItemToCategory(200452, L:G("Reputation Insignia")) --Dragonscale Expedition Insignia (Blue)
+categories:AddItemToCategory(201921, L:G("Reputation Insignia")) --Dragonscale Expedition Insignia (Purple)
+categories:AddItemToCategory(202091, L:G("Reputation Insignia")) --Dragonscale Expedition Insignia (Purple - Weekly Reward)
+
+--Iskaara Tuskarr
+categories:AddItemToCategory(200287, L:G("Reputation Insignia")) --Iskaara Tuskarr Insignia (Green)
+categories:AddItemToCategory(200453, L:G("Reputation Insignia")) --Iskaara Tuskarr Insignia (Blue)
+categories:AddItemToCategory(201922, L:G("Reputation Insignia")) --Iskaara Tuskarr Insignia (Purple)
+categories:AddItemToCategory(202092, L:G("Reputation Insignia")) --Iskaara Tuskarr Insignia (Purple - Weekly Event)
+
+--Maruuk Centaur
+categories:AddItemToCategory(200288, L:G("Reputation Insignia")) --Maruuk Centaur Insignia (Green)
+categories:AddItemToCategory(200454, L:G("Reputation Insignia")) --Maruuk Centaur Insignia (Blue)
+categories:AddItemToCategory(201923, L:G("Reputation Insignia")) --Maruuk Centaur Insignia (Purple)
+categories:AddItemToCategory(202094, L:G("Reputation Insignia")) --Maruuk Centaur Insignia (Purple - Weekly Event)
+
+--Valdrakken Accord
+categories:AddItemToCategory(200289, L:G("Reputation Insignia")) --Valdrakken Accord Insignia (Green)
+categories:AddItemToCategory(200455, L:G("Reputation Insignia")) --Valdrakken Accord Insignia (Blue)
+categories:AddItemToCategory(201924, L:G("Reputation Insignia")) --Valdrakken Accord Insignia (Purple)
+categories:AddItemToCategory(202093, L:G("Reputation Insignia")) --Valdrakken Accord Insignia (Purple - Weekly Event)
+
+--Loamm Niffen
+categories:AddItemToCategory(205365, L:G("Reputation Insignia")) --Loamm Niffen Insignia (Green)
+categories:AddItemToCategory(205342, L:G("Reputation Insignia")) --Loamm Niffen Insignia (Blue)
+categories:AddItemToCategory(205985, L:G("Reputation Insignia")) --Loamm Niffen Insignia (Purple)
+categories:AddItemToCategory(210422, L:G("Reputation Insignia")) --Loamm Niffen Insignia (Purple - Weekly Event)
+
+--Dream Wardens
+categories:AddItemToCategory(210419, L:G("Reputation Insignia")) --Dream Wardens Insignia (Green)
+categories:AddItemToCategory(210420, L:G("Reputation Insignia")) --Dream Wardens Insignia (Blue)
+categories:AddItemToCategory(210421, L:G("Reputation Insignia")) --Dream Wardens Insignia (Purple - A Worthy Ally quest)
+categories:AddItemToCategory(211416, L:G("Reputation Insignia")) --Dream Wardens Insignia (Purple - Weekly Event)
+categories:AddItemToCategory(211423, L:G("Reputation Insignia")) --Dream Wardens Insignia (Purple)
+categories:AddItemToCategory(210417, L:G("Reputation Insignia")) --Dream Wardens Insignia (Purple - Drop)
