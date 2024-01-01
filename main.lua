6e62c2411d0881aee0061fd3d6d43a5b4fb34532
Zazou89 <155338110+Zazou89@users.noreply.github.com>
2024-01-01 13:57:06 +0100

    Adding "Turn in" items

--- main.lua
+++ main.lua
@@ -12,24 +12,32 @@ categories:AddItemToCategory(200285, L:G("Reputation Insignia")) --Dragonscale E
 categories:AddItemToCategory(200452, L:G("Reputation Insignia")) --Dragonscale Expedition Insignia (Blue)
 categories:AddItemToCategory(201921, L:G("Reputation Insignia")) --Dragonscale Expedition Insignia (Purple)
 categories:AddItemToCategory(202091, L:G("Reputation Insignia")) --Dragonscale Expedition Insignia (Purple - Weekly Reward)
+categories:AddItemToCategory(200443, L:G("Reputation Insignia")) --Dragon Isles Artifact (BoA)
+categories:AddItemToCategory(192055, L:G("Reputation Insignia")) --Dragon Isles Artifact (BoP)
 
 --Iskaara Tuskarr
 categories:AddItemToCategory(200287, L:G("Reputation Insignia")) --Iskaara Tuskarr Insignia (Green)
 categories:AddItemToCategory(200453, L:G("Reputation Insignia")) --Iskaara Tuskarr Insignia (Blue)
 categories:AddItemToCategory(201922, L:G("Reputation Insignia")) --Iskaara Tuskarr Insignia (Purple)
 categories:AddItemToCategory(202092, L:G("Reputation Insignia")) --Iskaara Tuskarr Insignia (Purple - Weekly Event)
+categories:AddItemToCategory(200449, L:G("Reputation Insignia")) --Sacred Tuskarr Totem (BoA)
+categories:AddItemToCategory(200071, L:G("Reputation Insignia")) --Sacred Tuskarr Totem (BoP)
 
 --Maruuk Centaur
 categories:AddItemToCategory(200288, L:G("Reputation Insignia")) --Maruuk Centaur Insignia (Green)
 categories:AddItemToCategory(200454, L:G("Reputation Insignia")) --Maruuk Centaur Insignia (Blue)
 categories:AddItemToCategory(201923, L:G("Reputation Insignia")) --Maruuk Centaur Insignia (Purple)
 categories:AddItemToCategory(202094, L:G("Reputation Insignia")) --Maruuk Centaur Insignia (Purple - Weekly Event)
+categories:AddItemToCategory(200447, L:G("Reputation Insignia")) --Centaur Hunting Trophy (BoA)
+categories:AddItemToCategory(200093, L:G("Reputation Insignia")) --Centaur Hunting Trophy (BoP)
 
 --Valdrakken Accord
 categories:AddItemToCategory(200289, L:G("Reputation Insignia")) --Valdrakken Accord Insignia (Green)
 categories:AddItemToCategory(200455, L:G("Reputation Insignia")) --Valdrakken Accord Insignia (Blue)
 categories:AddItemToCategory(201924, L:G("Reputation Insignia")) --Valdrakken Accord Insignia (Purple)
 categories:AddItemToCategory(202093, L:G("Reputation Insignia")) --Valdrakken Accord Insignia (Purple - Weekly Event)
+categories:AddItemToCategory(200450, L:G("Reputation Insignia")) --Titan Relic (BoA)
+categories:AddItemToCategory(199906, L:G("Reputation Insignia")) --Titan Relic (BoP)
 
 --Loamm Niffen
 categories:AddItemToCategory(205365, L:G("Reputation Insignia")) --Loamm Niffen Insignia (Green)
@@ -43,4 +51,4 @@ categories:AddItemToCategory(210420, L:G("Reputation Insignia")) --Dream Wardens
 categories:AddItemToCategory(210421, L:G("Reputation Insignia")) --Dream Wardens Insignia (Purple - A Worthy Ally quest)
 categories:AddItemToCategory(211416, L:G("Reputation Insignia")) --Dream Wardens Insignia (Purple - Weekly Event)
 categories:AddItemToCategory(211423, L:G("Reputation Insignia")) --Dream Wardens Insignia (Purple)
-categories:AddItemToCategory(211417, L:G("Reputation Insignia")) --Dream Wardens Insignia (Purple - Drop)
+categories:AddItemToCategory(210417, L:G("Reputation Insignia")) --Dream Wardens Insignia (Purple - Drop)
